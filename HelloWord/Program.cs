fc3adbf6ea8f492a923eacf8b6f59bb15a92b331
Redstoneur <alfa.aog36@gmail.com>
2024-12-03 16:02:44 +0100

    Update Program.cs
    Evolution du helloWord via la Program

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+namespace HelloWord;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
